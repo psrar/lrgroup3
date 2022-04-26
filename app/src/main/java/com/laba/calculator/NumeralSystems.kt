44af6826c5faabdece49e3a6dd468fb267326a57
psrar <pavel.sima203@gmail.com>
2022-04-27 01:16:55 +0300

    Разработан начальный макет конвертера

--- app/src/main/java/com/laba/calculator/NumeralSystems.kt
+++ app/src/main/java/com/laba/calculator/NumeralSystems.kt
@@ -2,10 +2,15 @@ package com.laba.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NumeralSystems : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numeral_systems)
+
+        for (i in 0..10) {
+            var button = findViewById<Button>(R.id.calcButton0);
+        }
     }
 }
