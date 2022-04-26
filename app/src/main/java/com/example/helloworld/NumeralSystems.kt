ffece2575c8941c35d2665dea42c796d9a3ffb85
psrar <pavel.sima203@gmail.com>
2022-04-26 22:23:54 +0300

    Создание Activity перевода чисел в разные системы счисления

--- app/src/main/java/com/example/helloworld/NumeralSystems.kt
+++ app/src/main/java/com/example/helloworld/NumeralSystems.kt
@@ -2,13 +2,10 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class NumeralSystems : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_numeral_systems)
     }
 }
